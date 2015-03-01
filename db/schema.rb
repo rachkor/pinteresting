cebe92bcd4e9f9ce63de744d879d081936fb4711
Rachael Korinek <rachael.korinek@gmail.com>
2015-03-01 14:06:54 -0800

    add username to usersignup and account settings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150301203535) do
+ActiveRecord::Schema.define(version: 20150301210638) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
@@ -32,9 +32,11 @@ ActiveRecord::Schema.define(version: 20150301203535) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end

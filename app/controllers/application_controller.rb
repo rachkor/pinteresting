2af78dca7bc5742603fc2374dcd97c8ccb985787
Rachael Korinek <rachael.korinek@gmail.com>
2015-03-08 12:14:14 -0700

    edit page updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+protected
     
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
